508a01c7e1eebade18b80f8302a5940009ec9bed
KTachibanaM <whj19931115@gmail.com>
2020-02-08 17:07:59 -0800

    form look better

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react'
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import withStyles from "@material-ui/core/styles/withStyles";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControl from "@material-ui/core/FormControl";
+import formControlStyles from "../styles/formControl"
+import Container from "@material-ui/core/Container";
 
 class Settings extends Component {
   constructor(props) {
@@ -11,31 +16,38 @@ class Settings extends Component {
   }
 
   render() {
+    const { classes } = this.props;
     return (
-      <form>
-        <TextField
-          defaultValue={this.state.repoUrl}
-          label="Repository URL"
-          onChange={(e, newValue) => {
+      <Container>
+        <FormGroup row={true}>
+          <FormControl className={classes.formControl}>
+            <TextField
+              defaultValue={this.state.repoUrl}
+              label="Repository URL"
+              onChange={(e, newValue) => {
+                e.preventDefault();
+                this.setState({
+                  repoUrl: newValue
+                })
+              }}
+            />
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <Button variant="contained" color="primary" onClick={e => {
+              e.preventDefault();
+              this.props.setRepoUrl(this.state.repoUrl)
+            }}>Confirm</Button>
+          </FormControl>
+        </FormGroup>
+        <FormControl>
+          <Button variant="contained" color="secondary" onClick={e => {
             e.preventDefault();
-            this.setState({
-              repoUrl: newValue
-            })
-          }}
-        />
-        <br/>
-        <Button variant="contained" color="primary" onClick={e => {
-          e.preventDefault();
-          this.props.setRepoUrl(this.state.repoUrl)
-        }}>Confirm</Button>
-        <br/>
-        <Button variant="contained" color="secondary" onClick={e => {
-          e.preventDefault();
-          window.close()
-        }}>Exit</Button>
-      </form>
+            window.close()
+          }}>Exit</Button>
+        </FormControl>
+      </Container>
     )
   }
 }
 
-export default Settings
+export default withStyles(formControlStyles)(Settings)
---
+++ src/styles/formControl.js
@@ -0,0 +1,7 @@
+import { createStyles } from "@material-ui/core";
+
+export default theme => createStyles({
+  formControl: {
+    margin: theme.spacing(1),
+  }
+});
