964f2b435cfb47b5ba9d77292888fd4062b340a9
KTachibanaM <whj19931115@gmail.com>
2020-02-09 19:37:15 -0800

    empty view

--- src/Components/Favorites.js
+++ src/Components/Favorites.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import Emoticon from './Emoticon'
 import List from '@material-ui/core/List';
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
 const Favorites = (props) => {
-  const items = props.getFavorites().map((item, i) => {
+  const favorites = props.getFavorites();
+  if (!favorites || favorites.length === 0) {
+    return (
+      <Grid container>
+        <Grid item xs={12}>
+          <Typography variant='h4'>
+            (〜￣△￣)〜
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant='h6'>
+            You don't have any bookmarks
+          </Typography>
+        </Grid>
+      </Grid>
+    )
+  }
+  const items = favorites.map((item, i) => {
     return (
       <Emoticon
         key={i}
