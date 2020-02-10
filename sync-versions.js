19264902b94830af05b3934e50f094d0de0c958f
KTachibanaM <whj19931115@gmail.com>
2020-02-09 18:50:14 -0800

    add electron

--- sync-versions.js
+++ sync-versions.js
@@ -1,17 +1,18 @@
 const packageJson = require('./package.json');
 const fs = require('fs');
+const path = require('path');
 
 const version = packageJson['version'];
 const vercode = packageJson['vercode'];
 console.log(`Syncing to version ${version} and vercode ${vercode}`);
 
 console.log("Syncing Android version");
-const gradlePropertiesPath = './android/app/gradle.properties';
+const gradlePropertiesPath = path.join('android', 'app', 'gradle.properties');
 fs.writeFileSync(gradlePropertiesPath,`android.versionName=${version}
 android.versionCode=${vercode}`);
 
 console.log("Syncing iOS version");
-const projectPbxprojPath = './ios/App/App.xcodeproj/project.pbxproj';
+const projectPbxprojPath = path.join('ios', 'App', 'App.xcodeproj', 'project.pbxproj');
 let projectPbxproj = fs.readFileSync(projectPbxprojPath, 'utf-8');
 projectPbxproj = projectPbxproj.replace(
   /CURRENT_PROJECT_VERSION = [0-9]*;/g,
@@ -22,3 +23,9 @@ projectPbxproj = projectPbxproj.replace(
   `MARKETING_VERSION = ${version};`
 );
 fs.writeFileSync(projectPbxprojPath, projectPbxproj);
+
+console.log("Syncing Electron version");
+const electronPackageJsonPath = path.join('electron', 'package.json');
+let electronPackageJson = JSON.parse(fs.readFileSync(electronPackageJsonPath, 'utf-8'));
+electronPackageJson['version'] = version;
+fs.writeFileSync(electronPackageJsonPath, JSON.stringify(electronPackageJson, null, 2));
