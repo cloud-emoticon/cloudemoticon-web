acb41800e2d0c587a94cc4117f52ceeea7646595
KTachibanaM <whj19931115@gmail.com>
2023-06-11 12:07:32 -0700

    remove exit app

--- src/Pages/Settings.js
+++ src/Pages/Settings.js
@@ -10,12 +10,6 @@ class Settings extends Component {
         <ListItem>
           <ListItemText primary="Version" secondary={window.ceVersion}/>
         </ListItem>
-        <ListItem onClick={e => {
-          e.preventDefault();
-          window.close()
-        }}>
-          <ListItemText primary="Exit" secondary="Exit the app"/>
-        </ListItem>
       </List>
     )
   }
